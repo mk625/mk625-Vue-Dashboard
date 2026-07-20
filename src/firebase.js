1711f34503e4e51aedd207a6f5528645fd745897
Muthu Kumar <88616865+mk625@users.noreply.github.com>
2026-07-20 17:32:46 +0530

    code update

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,24 @@
-
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCimFL-JRiKVVEnutqGS7JgFDIrtoYpP3s",
-    authDomain: "react-dashboard-16f78.firebaseapp.com",
-    projectId: "react-dashboard-16f78",
-    storageBucket: "react-dashboard-16f78.firebasestorage.app",
-    messagingSenderId: "611213212394",
-    appId: "1:611213212394:web:b10f924f52376579ddf593",
-    measurementId: "G-Z1L5KSSL0V"
+  apiKey: "AIzaSyA47BuLTw3l6p0ErlacACIAUX5UoVtIGss",
+  authDomain: "employees-management-eef03.firebaseapp.com",
+  projectId: "employees-management-eef03",
+  storageBucket: "employees-management-eef03.firebasestorage.app",
+  messagingSenderId: "324005070473",
+  appId: "1:324005070473:web:79d2b62dc60e77ecba6858",
+  measurementId: "G-6J4CTSCFKW"
 };
 
+
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
