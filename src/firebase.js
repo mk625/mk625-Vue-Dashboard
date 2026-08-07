e542fbf9b03705c656555f5d0af9076b478a0cd1
Muthu Kumar <88616865+mk625@users.noreply.github.com>
2026-08-07 11:36:40 +0530

    code update

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,4 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
@@ -8,13 +9,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA47BuLTw3l6p0ErlacACIAUX5UoVtIGss",
-  authDomain: "employees-management-eef03.firebaseapp.com",
-  projectId: "employees-management-eef03",
-  storageBucket: "employees-management-eef03.firebasestorage.app",
-  messagingSenderId: "324005070473",
-  appId: "1:324005070473:web:79d2b62dc60e77ecba6858",
-  measurementId: "G-6J4CTSCFKW"
+    apiKey: "AIzaSyA47BuLTw3l6p0ErlacACIAUX5UoVtIGss",
+    authDomain: "employees-management-eef03.firebaseapp.com",
+    projectId: "employees-management-eef03",
+    storageBucket: "employees-management-eef03.firebasestorage.app",
+    messagingSenderId: "324005070473",
+    appId: "1:324005070473:web:79d2b62dc60e77ecba6858",
+    measurementId: "G-6J4CTSCFKW"
 };
 
 
@@ -23,3 +24,4 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 export default db;
+
